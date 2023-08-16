b03eb0a4533fef677f2b31205810afb807ff3cc3
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2023-08-16 21:59:49 +0100

    fix(monitoring): by default print error to log if exception capturer is not set

--- monitoring/exception.go
+++ monitoring/exception.go
@@ -1,5 +1,7 @@
 package monitoring
 
+import "log"
+
 var captureException func(err error) Event
 
 // SetExceptionCapturer sets a function that will be called to capture exception.
@@ -10,13 +12,21 @@ func SetExceptionCapturer(capture func(err error) Event) {
 	captureException = capture
 }
 
+// Event represents an event that was captured my monitoring subsystem.
 type Event struct {
 	ID string
 }
 
+// CaptureException captures exception and returns event ID.
 func CaptureException(err error) Event {
 	if captureException == nil {
-		panic("exception capturer is not set, call monitoring.SetExceptionCapturer(capture func(err error)) in you app initialization code")
+		log.Println("WARNING:", "Exception capturer is not set. Call monitoring.SetExceptionCapturer(capture func(err error)) in you app initialization code")
+
+		captureException = func(err error) Event {
+			log.Println("ERROR:", err)
+			return Event{}
+		}
 	}
+
 	return captureException(err)
 }
