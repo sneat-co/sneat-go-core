c22fa695566612d30cdf25354b7aba44ad17152b
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2023-09-18 06:55:15 +0100

    fix(apicore): NoContentAuthRequired added

--- apicore/verify/defaults.go
+++ apicore/verify/defaults.go
@@ -1,12 +1,23 @@
 package verify
 
+// DefaultJsonWithAuthRequired is a request verification that requires authentication and expects JSON content
+// with default min = MinJSONRequestSize and max = DefaultMaxJSONRequestSize for content length
 var DefaultJsonWithAuthRequired = Request(
 	AuthenticationRequired(true),
 	MinimumContentLength(MinJSONRequestSize),
 	MaximumContentLength(DefaultMaxJSONRequestSize),
 )
 
+// DefaultJsonWithNoAuthRequired is a request verification that does not require authentication and expects JSON content
+// with default min = MinJSONRequestSize and max = DefaultMaxJSONRequestSize for content length
 var DefaultJsonWithNoAuthRequired = Request(
 	MinimumContentLength(MinJSONRequestSize),
 	MaximumContentLength(DefaultMaxJSONRequestSize),
+)
+
+// NoContentAuthRequired is a request verification that requires authentication and expects no content
+var NoContentAuthRequired = Request(
+	AuthenticationRequired(true),
+	MinimumContentLength(0),
+	MaximumContentLength(0),
 )
--- apicore/verify/defaults_test.go
+++ apicore/verify/defaults_test.go
@@ -16,3 +16,9 @@ func TestDefaultJsonWithNoAuthRequired(t *testing.T) {
 	assert.Equal(t, MinJSONRequestSize, DefaultJsonWithNoAuthRequired.MinimumContentLength())
 	assert.Equal(t, DefaultMaxJSONRequestSize, DefaultJsonWithNoAuthRequired.MaximumContentLength())
 }
+
+func TestNoContentAuthRequired(t *testing.T) {
+	assert.True(t, NoContentAuthRequired.AuthenticationRequired())
+	assert.Equal(t, int64(0), NoContentAuthRequired.MinimumContentLength())
+	assert.Equal(t, int64(0), NoContentAuthRequired.MaximumContentLength())
+}
