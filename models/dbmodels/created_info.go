bf8333790588caa6272c549f698ae407c3f5f638
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2023-10-26 21:20:38 +0100

    feat: removed 'created.at' from CreatedInfo - use 'createdAt' instead

--- models/dbmodels/created_info.go
+++ models/dbmodels/created_info.go
@@ -2,19 +2,13 @@ package dbmodels
 
 import (
 	"github.com/strongo/validation"
-	"time"
 )
 
 type CreatedInfo struct {
-	At time.Time `json:"at,omitempty"  firestore:"at,omitempty"`
-	//
 	Client RemoteClientInfo `json:"client,omitempty"  firestore:"client,omitempty"`
 }
 
 func (v CreatedInfo) Validate() error {
-	if v.At.IsZero() {
-		return validation.NewErrRecordIsMissingRequiredField("at")
-	}
 	if err := v.Client.Validate(); err != nil {
 		return validation.NewErrBadRecordFieldValue("client", err.Error())
 	}
