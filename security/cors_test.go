b939b539802eeb1c3460599b65ec7b418577c08f
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2023-11-05 12:54:40 +0000

    fix: TestIsSupportedOrigin checks only default hosts

--- security/cors_test.go
+++ security/cors_test.go
@@ -1,9 +1,17 @@
 package security
 
-import "testing"
+import (
+	core "github.com/sneat-co/sneat-go-core"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestKnownHosts(t *testing.T) {
+	assert.Equal(t, []string{"localhost:4200"}, core.KnownHosts)
+}
 
 func TestIsSupportedOrigin(t *testing.T) {
-	for i, s := range []string{"", "http://localhost:8100", "https://dailyscrum.app"} {
+	for i, s := range []string{"", "http://localhost:8100"} {
 		if !IsSupportedOrigin(s) {
 			t.Errorf("string #%d [%v] should be a supported origin", i, s)
 		}
