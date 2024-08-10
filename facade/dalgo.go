7a31dc88cc72ab602a4d76c73209c151d84ccc04
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2024-08-10 10:54:09 +0100

    feat(facade): RunReadwriteTransaction()

--- facade/dalgo.go
+++ facade/dalgo.go
@@ -10,3 +10,12 @@ import (
 var GetDatabase = func(ctx context.Context) (dal.DB, error) {
 	return nil, fmt.Errorf("%w: facade.GetDatabase(context.Context) (dal.DB, error)", ErrNotInitialized)
 }
+
+// RunReadwriteTransaction is a helper wrapper that created a facade DB instance and runs a transaction
+func RunReadwriteTransaction(ctx context.Context, f func(ctx context.Context, tx dal.ReadwriteTransaction) error) (err error) {
+	var db dal.DB
+	if db, err = GetDatabase(ctx); err != nil {
+		return err
+	}
+	return db.RunReadwriteTransaction(ctx, f)
+}
