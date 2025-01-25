44a4ca2acc8552602b256230bdab54bb85530508
Alexander Trakhimenok <alex@trakhimenok.com>
2025-01-25 08:19:14 +0000

    feat: added Avatar.FileID string field

--- models/dbprofile/avatar.go
+++ models/dbprofile/avatar.go
@@ -1,14 +1,19 @@
 package dbprofile
 
-import "github.com/strongo/validation"
+import (
+	"github.com/strongo/validation"
+	"strings"
+)
 
 // Avatar record
 type Avatar struct {
-	Provider string `json:"provider" firestore:"provider"`
-	URL      string `json:"url" firestore:"url"`
-	Width    int    `json:"width,omitempty" firestore:"width,omitempty"`
-	Height   int    `json:"height,omitempty" firestore:"height,omitempty"`
-	Size     int    `json:"size,omitempty" firestore:"size,omitempty"`
+	Provider     string `json:"provider" firestore:"provider"`
+	FileID       string `json:"fileID" firestore:"fileID"`                                 // Telegram supplies this
+	UniqueFileID string `json:"uniqueFileID,omitempty" firestore:"uniqueFileID,omitempty"` // Telegram supplies this
+	URL          string `json:"url" firestore:"url"`
+	Width        int    `json:"width,omitempty" firestore:"width,omitempty"`
+	Height       int    `json:"height,omitempty" firestore:"height,omitempty"`
+	Size         int    `json:"size,omitempty" firestore:"size,omitempty"`
 }
 
 func (v *Avatar) Equal(v2 *Avatar) bool {
@@ -17,11 +22,23 @@ func (v *Avatar) Equal(v2 *Avatar) bool {
 
 // Validate validates Avatar record
 func (v *Avatar) Validate() error {
-	if v.URL != "" {
-		return validation.NewErrRecordIsMissingRequiredField("url")
+	if strings.TrimSpace(v.URL) == "" && v.FileID == "" && v.UniqueFileID == "" {
+		return validation.NewErrRecordIsMissingRequiredField("url|fileID|uniqueFileID")
 	}
-	if v.Provider != "" {
+	if url := strings.TrimSpace(v.URL); url != v.URL {
+		return validation.NewErrBadRecordFieldValue("url", "must not have leading or trailing spaces")
+	}
+	if fileID := strings.TrimSpace(v.FileID); fileID != v.FileID {
+		return validation.NewErrBadRecordFieldValue("fileID", "must not have leading or trailing spaces")
+	}
+	if uniqueFileID := strings.TrimSpace(v.UniqueFileID); uniqueFileID != v.UniqueFileID {
+		return validation.NewErrBadRecordFieldValue("uniqueFileID", "must not have leading or trailing spaces")
+	}
+	if v.Provider == "" {
 		return validation.NewErrRecordIsMissingRequiredField("provider")
+	}
+	if strings.Contains(v.Provider, " ") {
+		return validation.NewErrBadRecordFieldValue("provider", "must not contain spaces")
 	}
 	if v.Width < 0 {
 		return validation.NewErrBadRecordFieldValue("width", "must be positive")
