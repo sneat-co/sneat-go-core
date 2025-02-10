1ea9ba625801a6966fd8d10f6ab549a428bebe96
Alexander Trakhimenok <alex@trakhimenok.com>
2025-02-10 10:45:54 +0000

    fix(deps): github.com/golang/mock => go.uber.org/mock

--- facade/run_transaction.go
+++ facade/run_transaction.go
@@ -9,12 +9,17 @@ import (
 
 // RunReadwriteTransaction is a helper wrapper that created a facade DB instance and runs a transaction
 func RunReadwriteTransaction(ctx context.Context, f func(ctx context.Context, tx dal.ReadwriteTransaction) error, options ...dal.TransactionOption) (err error) {
-	if _, ok := ctx.Deadline(); !ok {
-		ctx, _ = consts4dal.WithDefaultDeadLine(ctx)
-	}
 	var db dal.DB
 	if db, err = GetSneatDB(ctx); err != nil {
 		return fmt.Errorf("failed to GetSneatDB() inside facade.RunReadwriteTransaction(): %w", err)
+	}
+	return RunReadwriteTransactionWithDB(ctx, db, f, options...)
+}
+
+// RunReadwriteTransactionWithDB is a helper wrapper that created a facade DB instance and runs a transaction
+func RunReadwriteTransactionWithDB(ctx context.Context, db dal.DB, f func(ctx context.Context, tx dal.ReadwriteTransaction) error, options ...dal.TransactionOption) (err error) {
+	if _, ok := ctx.Deadline(); !ok {
+		ctx, _ = consts4dal.WithDefaultDeadLine(ctx)
 	}
 	return db.RunReadwriteTransaction(ctx, f, options...)
 }
