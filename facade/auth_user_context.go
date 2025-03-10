baa506f6bb7d536e78f160a828a272537c41da3f
Alexander Trakhimenok <alex@trakhimenok.com>
2025-03-10 12:35:47 +0000

    feat: restores AuthContext interface

--- facade/auth_user_context.go
+++ facade/auth_user_context.go
@@ -4,8 +4,9 @@ import (
 	"context"
 )
 
-// AuthContextObsolete defines auth context interface
-// deprecated: use ContextWithUser instead?
-type AuthContextObsolete interface {
-	User(ctx context.Context, authRequired bool) (userContext, error)
+// AuthContext defines auth context provider that can be used to retrieve user context.
+// For example, take Firebase ID token from request header, verify, return user context with a user ID provided by token
+// This is implemented in github.com/sneat-co/sneat-go-firebase module.
+type AuthContext interface {
+	User(ctx context.Context, authRequired bool) (UserContext, error)
 }
