e13634a9b8b8374bc937c69baf3dea1367f40f29
Alexander Trakhimenok <alex@trakhimenok.com>
2025-03-16 07:31:50 +0000

    fix: SharedToMap & SharedFromMap

--- coretypes/with_shared_map.go
+++ coretypes/with_shared_map.go
@@ -2,7 +2,9 @@ package coretypes
 
 import (
 	"fmt"
+	"github.com/dal-go/dalgo/update"
 	"github.com/strongo/validation"
+	"slices"
 	"strings"
 )
 
@@ -32,43 +34,101 @@ func (v ShareDetail) Validate() error {
 	return nil
 }
 
-// SharedMap is map[SpaceID]map[EntityRefWithoutSpaceID]ShareDetail
-type SharedMap map[string]map[string]ShareDetail
+// SharedToMap is map[EntityRefWithoutSpaceID]map[SpaceID]ShareDetail
+type SharedToMap map[string]map[string]ShareDetail
+
+// SharedFromMap is map[SpaceID]map[EntityRefWithoutSpaceID]ShareDetail
+type SharedFromMap map[string]map[string]ShareDetail
 
 type WithSharedMap struct {
-	SharedTo   SharedMap `json:"sharedTo,omitempty" firestore:"sharedTo,omitempty"`
-	SharedFrom SharedMap `json:"sharedFrom,omitempty" firestore:"sharedFrom,omitempty"`
+	SharedTo   SharedToMap   `json:"sharedTo,omitempty" firestore:"sharedTo,omitempty"`
+	SharedFrom SharedFromMap `json:"sharedFrom,omitempty" firestore:"sharedFrom,omitempty"`
 }
 
-func (v WithSharedMap) Validate() error {
-	validate := func(fieldName string, shared SharedMap) error {
-		for spaceID, refs := range shared {
-			if strings.TrimSpace(string(spaceID)) != string(spaceID) {
-				return validation.NewErrBadRecordFieldValue("fieldName", "spaceID key has leading or trailing spaces")
+func (v *WithSharedMap) Validate() error {
+	if err := validateSharedTo("sharedTo", v.SharedTo); err != nil {
+		return err
+	}
+	if err := validateSharedFrom("sharedFrom", v.SharedFrom); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (v *WithSharedMap) AddSharedTo(entityRef EntityRefWithoutSpaceID, spaceID string, shareDetail ShareDetail) (u update.Update, err error) {
+	if entityRef == "" {
+		panic("entityRef is a required parameter")
+	}
+	if spaceID == "" {
+		panic("spaceID is a required parameter")
+	}
+	if v.SharedTo == nil {
+		v.SharedTo = make(SharedToMap, 1)
+	}
+	spaces := v.SharedTo[string(entityRef)]
+	if spaces == nil {
+		spaces = make(map[string]ShareDetail, 1)
+		v.SharedTo[string(entityRef)] = spaces
+	}
+	if existing, ok := spaces[spaceID]; ok && existing.ByUserID == shareDetail.ByUserID && slices.Equal(existing.Permissions, shareDetail.Permissions) {
+		return nil, nil
+	}
+	spaces[spaceID] = shareDetail
+	return update.ByFieldPath([]string{"sharedTo", string(entityRef), spaceID}, shareDetail), nil
+}
+
+func (v *WithSharedMap) AddSharedFrom(spaceID string, entityRef EntityRefWithoutSpaceID, shareDetail ShareDetail) (u update.Update, err error) {
+	if spaceID == "" {
+		panic("spaceID is a required parameter")
+	}
+	if entityRef == "" {
+		panic("entityRef is a required parameter")
+	}
+	if v.SharedFrom == nil {
+		v.SharedFrom = make(SharedFromMap, 1)
+	}
+	entities := v.SharedFrom[spaceID]
+	if entities == nil {
+		entities = make(map[string]ShareDetail, 1)
+		v.SharedTo[string(entityRef)] = entities
+	}
+	if existing, ok := entities[string(entityRef)]; ok && existing.ByUserID == shareDetail.ByUserID && slices.Equal(existing.Permissions, shareDetail.Permissions) {
+		return nil, nil
+	}
+	entities[spaceID] = shareDetail
+	return update.ByFieldPath([]string{"sharedFrom", spaceID, string(entityRef)}, shareDetail), nil
+}
+
+func validateSharedFrom(fieldName string, sharedFrom SharedFromMap) (err error) {
+	for spaceID, refs := range sharedFrom {
+		if strings.TrimSpace(string(spaceID)) != string(spaceID) {
+			return validation.NewErrBadRecordFieldValue("fieldName", "spaceID key has leading or trailing spaces")
+		}
+		for entityRef, shareDetail := range refs {
+			if err = EntityRefWithoutSpaceID(entityRef).Validate(); err != nil {
+				return validation.NewErrBadRecordFieldValue(fmt.Sprintf("%s.%s", fieldName, spaceID), err.Error())
 			}
-			for entityRef, shareDetail := range refs {
-				if err := EntityRefWithoutSpaceID(entityRef).Validate(); err != nil {
-					return validation.NewErrBadRecordFieldValue(fmt.Sprintf("%s.%s", fieldName, spaceID), err.Error())
-				}
-				if err := shareDetail.Validate(); err != nil {
-					return validation.NewErrBadRecordFieldValue(fmt.Sprintf("%s.%s.%s", fieldName, spaceID, entityRef), err.Error())
-				}
+			if err = shareDetail.Validate(); err != nil {
+				return validation.NewErrBadRecordFieldValue(fmt.Sprintf("%s.%s.%s", fieldName, spaceID, entityRef), err.Error())
 			}
 		}
-		return nil
 	}
-	if err := validate("sharedTo", v.SharedTo); err != nil {
-		return err
+	return nil
+}
+
+func validateSharedTo(fieldName string, sharedTo SharedToMap) (err error) {
+	for entityRef, refs := range sharedTo {
+		if strings.TrimSpace(string(entityRef)) != string(entityRef) {
+			return validation.NewErrBadRecordFieldValue("fieldName", "entityRef key has leading or trailing spaces")
+		}
+		if err = EntityRefWithoutSpaceID(entityRef).Validate(); err != nil {
+			return validation.NewErrBadRecordFieldValue(fmt.Sprintf("%s.%s", fieldName, entityRef), err.Error())
+		}
+		for spaceID, shareDetail := range refs {
+			if err = shareDetail.Validate(); err != nil {
+				return validation.NewErrBadRecordFieldValue(fmt.Sprintf("%s.%s.%s", fieldName, entityRef, spaceID), err.Error())
+			}
+		}
 	}
-	if err := validate("sharedFrom", v.SharedFrom); err != nil {
-		return err
-	}
-	// we can have reciprocal shares for calendar events when an event can be edit by a member of either space
-	// Consider if we should prevent this happening in other cases?
-	//for spaceID := range v.SharedTo {
-	//	if _, ok := v.SharedFrom[spaceID]; ok {
-	//		return validation.NewErrBadRecordFieldValue("sharedFrom&sharedTo", "same space userID in sharedTo and sharedFrom: "+spaceID)
-	//	}
-	//}
 	return nil
 }
